d3c6f4641edd3a04aba902665fc503b6b9721cb7
Aaron Hill <aaronthill16@gmail.com>
2017-01-18 17:00:40 -0500

    Allows a human input of a word to be guessed

--- app.rb
+++ app.rb
@@ -7,5 +7,10 @@ get '/' do
 	erb :choose_players
 end
 
-
+post '/chooseplayertype' do
+	session[:opponent] = params[:opponent]
+	if session[:opponent] == "Human"
+		erb :choose_word_human
+	end
+end
 
